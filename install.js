0b4d7f10a18eccb25443a286ba163a797e92310b
Trinyle <marcllphebade@gmail.com>
2024-12-03 11:44:48 +0100

    add install-btn

---
+++ install.js
@@ -0,0 +1,24 @@
+const installBtn = document.querySelector("#installButton")
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault()
+    deferredPrompt = event
+    installBtn.style.display = 'block'
+})
+
+installBtn/addEventListener('click', async (event) => {
+    event.preventDefault()
+    if(deferredPrompt){
+        deferredPrompt.prompt()
+        const choiceUser = await deferredPrompt.userChoice
+        if(choiceUser === 'accepted') {
+            installBtn.style.display = "none"
+        }
+        deferredPrompt = null
+    }
+})
+
+window.addEventListener('appinstalled', e => {
+    installBtn.style.display = "none"
+})
