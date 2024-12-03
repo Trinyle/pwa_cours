371c64428d7c10a3ef55b5da369384b401b891f4
Trinyle <marcllphebade@gmail.com>
2024-12-03 15:53:22 +0100

    notif change

--- notifications.js
+++ notifications.js
@@ -1,3 +1,23 @@
+const NotifMe = ()=> {
+    let myNotif =null
+    const options = {
+        body: "Plus que 2H de souffrance. ",
+        icon: "icons/pika.jpg",
+        url: "https://cepegra.be"
+    }
+    if(Notification.permission == "granted"){
+        myNotif = new Notification('Bien le bonjour étranger !', options)
+    }else{
+        Notification.requestPermission().then(permission => {
+            if(permission === "granted") {
+                console.log("NOtifications Obligatoire")
+                myNotification = new Notification('Oh, Thank You !', options)
+            }
+        })
+    }
+}
+NotifMe()
+
 if(Notification.permission !== 'granted') {
     if(confirm('Les Notifications sont Inevitable')){
         Notification.requestPermission().then(permission => {
